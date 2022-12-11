db467624a0362b5dd8b0be769507fd122fdb82ed
IrinaT87 <118206816+IrinaT87@users.noreply.github.com>
2022-12-11 19:17:55 +0300

    changed README

---
+++ cook_book.py
@@ -0,0 +1,18 @@
+with open ('ingridients.txt', encoding='utf-8') as file:
+    cook_book={}
+    for line in file:
+        dish_name=line
+        ingr_count=int(file.readline().strip())
+        ingridients=[]
+        for i in range(ingr_count):
+            ingridient=file.readline().strip()
+            prod, count, ed=ingridient.split(' | ')
+            ingridients.append({
+                'ingridient_name':prod,
+                'quantity': count,
+                'measure':ed
+            })
+        
+        file.readline()
+        cook_book[dish_name]=ingridients
+    print(cook_book)
