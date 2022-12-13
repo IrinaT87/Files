e92dd7e9c42f4616ba4291199d8f1ff17d1f02cb
IrinaT87 <118206816+IrinaT87@users.noreply.github.com>
2022-12-13 21:22:36 +0300

    added Files.py

---
+++ files.py
@@ -0,0 +1,23 @@
+from pprint import pprint
+
+files={}
+with open('text/t1.txt', 'rt', encoding='utf-8') as f1, open('text/t2.txt', encoding='utf-8') as f2, open('text/t3.txt', encoding='utf-8') as f3:
+    files['t1.txt']=f1.readlines()
+    files['t2.txt']=f2.readlines()
+    files['t3.txt']=f3.readlines()
+
+d1 = (sorted(files.items(), key=lambda item: len(item[1])))
+
+with open('text.txt', 'w', encoding='utf-8') as f:
+    
+    for el in d1:
+        f.write('\n')
+        f.write(str(el[0]))
+        f.write('\n')
+        f.write(str(len(el[1])))
+        f.write('\n')
+        f.write(str(el[1]))
+       
+        
+
+    
